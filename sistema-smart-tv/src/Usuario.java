af505b13999d85b53f841fe5a94ec71471481a2f
lucia.fernandes <lucia.fernandes@fiergs.org.br>
2024-07-25 09:28:23 -0300

    SmartTv classes

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,10 +1,10 @@
 public class Usuario {
+    
     public static void main(String[] args) throws Exception {
 
-        SmartTv smartTv;
-        smartTv = new SmartTv();
-        System.out.println("Tv ligada? " + smartTv.ligada);
-        //System.out.println("Canal atual: " + smartTv.canal);
+        SmartTv smartTv = new SmartTv();
+        System.out.println("Tv ligada? ");
+        System.out.println("Canal atual: " + smartTv.canal);
         //System.out.println("Volume atual: " + smartTv.volume);
         //smartTv.ligar();
         //System.out.println("Novo Status: Tv ligada? " + smartTv.ligada);       
