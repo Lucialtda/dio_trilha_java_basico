e4dea78115143e6afbb3b20599f11466bcc2d451
lucia.fernandes <lucia.fernandes@fiergs.org.br>
2024-07-25 12:23:10 -0300

    implementação dos canais

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -10,4 +10,24 @@ public class SmartTv{
     public void desligar () {
         ligada = false;
     }
+
+    public void aumentarVolume (){
+        volume++;
+    }
+
+    public void diminuirVolume (){
+        volume--;
+    }
+    public void aumentarCanal (){
+        canal++;
+    }
+
+    public void diminuirCanal (){
+        canal--;
+    }
+
+    public void mudarCanal (int novoCanal){
+        canal = novoCanal;
+    }
+
 }
