ffe594f36b279c4d30d744382163f508c61cf901
lucialtda <lucialtda@gmail.com>
2024-08-10 22:15:27 -0300

    feat:Implementado código com interfaces

---
+++ DesafioIphone/DesafioIphone/src/Usuario.java
@@ -0,0 +1,15 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        Iphone meuIphone = new Iphone();
+
+        meuIphone.atender();
+        meuIphone.ligar();
+        meuIphone.atualizarPagina();
+        meuIphone.adicionarNovaAba();
+        meuIphone.exibirPagina();
+        meuIphone.iniciarCorreioVoz();
+        meuIphone.pausar();
+        meuIphone.selecionarMusica();
+        meuIphone.tocar();
+    }
+}
